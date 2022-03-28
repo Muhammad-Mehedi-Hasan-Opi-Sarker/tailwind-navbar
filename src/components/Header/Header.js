61a80e169248fa495e976d5cf79824645de3c0cd
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-03-28 22:33:43 +0600

    four commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div>
             <Navbar></Navbar>
-            <h2 className='text-4xl'>Save most of your money.</h2>
+            <h2 className='text-4xl'>Save most  of your money.</h2>
             <p>By giving your money to me!!!</p>
         </div>
     );
